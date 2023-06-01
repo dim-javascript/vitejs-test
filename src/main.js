c290974e5666d49a0f80dfbc91647700e9369b5e
dim-javascript <40568708+dim-javascript@users.noreply.github.com>
2023-06-01 19:47:15 +0800

    2 files modified

--- src/main.js
+++ src/main.js
@@ -2,4 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 第三方平台的组件库
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+
+const app = createApp(App);
+
+app.use(ElementPlus, {
+  locale: zhCn
+})
+
+app.mount('#app')
